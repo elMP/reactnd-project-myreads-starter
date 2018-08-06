aaac190a2da3c663b5892dcb0751f6cd7b3ab347
kneazle <kneazle@yandex.ru>
2018-08-06 13:43:58 +0300

    Added Book component

--- src/BookShelf.js
+++ src/BookShelf.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react'
+import Book from './Book';
 
 class BookShelf extends Component {
-
     render() {
-        console.log(this.props);
+        //console.log(this.props);
+
         return (
             <div className="bookshelf">
             <h2 className="bookshelf-title">{this.props.shelf.title}</h2>
@@ -11,25 +12,7 @@ class BookShelf extends Component {
                 <ol className="books-grid">
                     {this.props.books.map((book)  => (
                         <li key={book.id}>
-                            <div className="book">
-                                <div className="book-top">
-                                    <div className="book-cover" style={{ width: 128, height: 193, 
-                                        backgroundImage: `url(${book.imageLinks.thumbnail})`
-                                        }}>
-                                    </div>
-                                    <div className="book-shelf-changer">
-                                        <select value={this.props.shelf.status}>
-                                            <option value="move" disabled>Move to...</option>
-                                            <option value="currentlyReading" >Currently Reading</option>
-                                            <option value="wantToRead">Want to Read</option>
-                                            <option value="read">Read</option>
-                                            <option value="none">None</option>
-                                        </select>
-                                    </div>
-                                </div>
-                                <div className="book-title">{book.title}</div>
-                                <div className="book-authors">{book.authors}</div>
-                            </div>
+                            <Book book={book} status={this.props.shelf.status}/>
                         </li>
                     ))}
                 </ol>
